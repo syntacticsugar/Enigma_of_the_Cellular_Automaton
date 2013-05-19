2b426e79e30ec0c67921829639f15500586140a8
RomyRomy :D <hello@romcreative.com>
2013-05-19 18:19:25 -0400

    Fixes event mouse coordinates in canvas.
    Use event.pageX and event.pageY to get position relative to the top of
    the page and offset this by the canvas' left and upper offsets.

--- public/javascripts/2canvas.js
+++ public/javascripts/2canvas.js
@@ -66,61 +66,18 @@ var canvasLeft = 0, canvasTop = 0;
 //canvasTop = canvas.offsetTop;
 
 canvas.addEventListener('mousedown', function(event) {
-  var xMouse = event.clientX - canvasLeft;
-  var yMouse = event.clientY - canvasTop;
+  /*
+  var canvasLeft = 0, canvasTop = 0;
+  (function() {
+    for (var element = canvas; element !== null; element = element.offsetParent) {
+      canvasLeft += element.offsetLeft;
+      canvasTop  += element.offsetTop;
+    }
+  })();
+  */
+  var xMouse = event.pageX - canvasLeft;
+  var yMouse = event.pageY - canvasTop;
   //var yMouse = event.clientY - canvasTop;
   // SUPER HACKER SUCKS!!!!!  why doesn't the correct y-point show up!?  scheisse!
   console.log( xMouse, yMouse );
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function renderGrid(gridPixelSize, color) {
-  ctx.strokeStyle = color;
-
-  // horizontal grid lines
-  for ( var i = 0; i <= canvas.height/gridPixelSize; i++ ) {
-    ctx.lineWidth = ( i % 10 === 0 ? 2.5 : 0.5 );
-    var y = i * gridPixelSize + 0.5;
-    drawLine(0,y, canvas.width,y);
-  }
-
-  // vertical grid lines
-  for ( var j = 0; j <= canvas.width/gridPixelSize; j++ ) {
-    ctx.lineWidth = ( i % 10 === 0 ? 2.5 : 0.5 );
-    var x = j * gridPixelSize + 0.5;
-    drawLine(x, 0, x, canvas.height);
-  }
-}
- function drawLine(x0, y0, x1, y1) {
-    ctx.beginPath();
-    ctx.moveTo(x0, y0);
-    ctx.lineTo(x1, y1);
-    ctx.closePath();
-    ctx.stroke();
-}
-// renderGrid(10, "rgb(169,169,169)");
