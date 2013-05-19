73f851d8b14778a4b49710d0b54a69e2cbe590d3
RomyRomy :D <hello@romcreative.com>
2013-05-19 19:58:49 -0400

    Adds `clearCell`, global `grrrid` array and `unit`

--- public/javascripts/2canvas.js
+++ public/javascripts/2canvas.js
@@ -23,9 +23,21 @@ function oHaiGrid(gridPixelSize, colour) {
   }
 }
 
+// global variables!!!!!!!!!!!!!!!!!!!!!
+var gridPixelSize = 10;
+var unit = gridPixelSize;  // nicky says to start with 30, a bigger square.
+
+var grrrid = [];
+for ( var j = 0; j < height/unit; j++ ) {
+  grrrid.push([]);
+  for ( var i = 0; i < width/unit; i++ ) {
+    grrrid[j].push(0);
+  }
+}
+
 // write a function that takes a coordinate like (73, 46)
 // and returns its corresponding cell (7, 4)
-function whereAmI(x,y,gridPixelSize) {
+function whereAmI(x,y) {
   var s = gridPixelSize;
   var i = Math.floor(x/s);
   var j = Math.floor(y/s);
@@ -34,14 +46,15 @@ function whereAmI(x,y,gridPixelSize) {
 
 // write a function that takes a cell like (7,4)
 // and fills it with gray
-function fillCell(i,j, gridPixelSize) {
+function fillCell(i,j) {
   // ok, gridPixelSize, plz kthnxBye!
-  var unit = gridPixelSize;  // nicky says to start with 30, a bigger square.
 
   ctx.fillStyle = "rgb(158,158,158)";
   ctx.fillStyle = "rgb(72,166,192)";
-  ctx.fillRect(i*unit, j*unit, unit, unit);  // (fillRect(70,40, 10,10)
+  ctx.fillRect(i*unit+1, j*unit+1, unit-1, unit-1);  // (fillRect(70,40, 10,10)
 }
+
+function clearCell(i,j) { ctx.clearRect(i*unit+1,j*unit+1,unit-1,unit-1) }
 
 function diagonal() {
   ctx.fillStyle = "rgb(72,166,192)";
@@ -62,22 +75,27 @@ var canvasLeft = 0, canvasTop = 0;
     }
 })();
 
+// This code may actually work since we only have ONE canvas element.
+// To do: experiment later!
 //canvasLeft = canvas.offsetLeft;
 //canvasTop = canvas.offsetTop;
 
 canvas.addEventListener('mousedown', function(event) {
-  /*
-  var canvasLeft = 0, canvasTop = 0;
-  (function() {
-    for (var element = canvas; element !== null; element = element.offsetParent) {
-      canvasLeft += element.offsetLeft;
-      canvasTop  += element.offsetTop;
-    }
-  })();
-  */
   var xMouse = event.pageX - canvasLeft;
   var yMouse = event.pageY - canvasTop;
   //var yMouse = event.clientY - canvasTop;
   // SUPER HACKER SUCKS!!!!!  why doesn't the correct y-point show up!?  scheisse!
   console.log( xMouse, yMouse );
 });
+
+canvas.addEventListener('mousedown', function(event) {
+  var xMouse = event.pageX - canvasLeft;
+  var yMouse = event.pageY - canvasTop;
+
+  var i = whereAmI(xMouse,yMouse)[0]
+  var j = whereAmI(xMouse,yMouse)[1]
+
+  fillCell(i,j);
+
+});
+
